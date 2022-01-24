a9f1765ffb139b5a7184a053ebc4c7a3b28553f2
fergalquinn77 <fergal@bourbon.ie>
2022-01-24 12:01:58 +0000

    Add event listeners for all buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,27 @@
-console.log("test");
+document.addEventListener("DOMContentLoaded", function () {
+
+    let buttons = document.getElementsByTagName("btn");
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            gameType = this.getAttribute("data-type");
+            runGame(gameType);
+        })
+    }
+    document.getElementById("start").onclick = startGame;
+    document.getElementById("reset").onclick = gameReset;
+});
+
+function runGame(){}
+
+function startGame(){}
+
+function updateMessage(){}
+
+function gameReset(){}
+
+function incrementScore(){}
+
+function checkScore(){}
+
+function endGame(){}
