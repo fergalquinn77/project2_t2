d370589ff22398a14dfea9559c927c012de86313
fergalquinn77 <fergal@bourbon.ie>
2022-01-24 12:35:42 +0000

    Adjust startGame function to display play buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
 function runGame(){}
 
-function startGame(){}
+function startGame(){
+    console.log("Start");
+    if (document.getElementById("difficulty").value === "easy") {
+        document.getElementById("easy").style.display = "flex";
+    } else {
+        document.getElementById("easy").style.display = "flex";
+        document.getElementById("difficult").style.display = "flex";
+    }
+    document.getElementById("start").style.display = "none";
+    document.getElementById("controls").style.display = "none";
+    document.getElementById("reset").style.display = "flex";
+    document.getElementById("scoreboard").style.display = "flex";
+    /* document.getElementByClassName("scoreboard").style.display = "flex";
+    document.getElementsByClassName("controls").style.display = "none"; */
+}
 
 function updateMessage(){}
 
